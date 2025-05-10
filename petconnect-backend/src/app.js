7deb5bb28cccce214466176858fac07e803cbae3
Emmanuel1399 <emma_1399@icloud.com>
2025-05-09 22:53:43 -0600

    Change the location of somefiles and start the development of the backend

---
+++ petconnect-backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const morgan = require('morgan')
+const routes = require('./routes')
+const errorHandler = require('./middlewares/errorHandler')
+
+const app = express();
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use('/api', routes)
+app.use(errorHandler)
+
+module.exports = app;
+
---
+++ petconnect-backend/src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const petRoutes = require('./pet.routes')
+const appointmenRoutes = require('./appointmen.routes')
+
+const router = express.Router();
+
+router.use('/pets', petRoutes)
+router.use('appointments', appointmenRoutes)
+
+module.exports = router;
